3b1d2ea32cac1037a7b2604450af287795dafec9
Gabriel Pelegri <gpelerii@myuax.com>
2023-04-25 10:46:28 +0200

    Finalizado

--- series.js
+++ series.js
@@ -1,5 +1,24 @@
 let seriesPending = ['Breaking Bug', 'Game of Drones', 'Boolean 99', 'Arrested Developer'];
+
 seriesPending.push('String Things');
-for(let i = 0; i < seriesPending.length; i++) {
-  console.log(seriesPending[i]);
+seriesPending.push('The Matrix');
+ 
+for (let i = 0; i < seriesPending.length; i++) {
+  console.log(`Serie #${i}: ${seriesPending[i]}`);
+ 
+  if (seriesPending[i] === 'Boolean 99') {
+    console.log('Found it!');
+  }
 }
+ 
+let seriesWatching = [];
+ 
+seriesWatching.push(seriesPending.shift());
+ 
+for (let i = 0; i < 4; i++) {
+  seriesWatching.push(seriesPending.shift());
+}
+ 
+seriesWatching.splice(0, seriesWatching.length);
+ 
+console.log(`seriesPending: ${seriesPending.length},   seriesWatching: ${seriesWatching.length}.`);
