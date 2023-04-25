cfec2320efaeec6e7b6828bd4a3615dac379f09b
Gabriel Pelegri <gpelerii@myuax.com>
2023-04-25 10:39:05 +0200

    first commit

---
+++ series.js
@@ -0,0 +1,5 @@
+let seriesPending = ['Breaking Bug', 'Game of Drones', 'Boolean 99', 'Arrested Developer'];
+seriesPending.push('String Things');
+for(let i = 0; i < seriesPending.length; i++) {
+  console.log(seriesPending[i]);
+}
